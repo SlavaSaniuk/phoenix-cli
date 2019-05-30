2d6868ab5287f4b5ef83553c25fd6a2ffd7d9eda
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-05-30 17:23:02 +0300

    Installed "log4j" as primary logging framework.

--- src/main/java/by/bsac/Main.java
+++ src/main/java/by/bsac/Main.java
@@ -1,5 +1,7 @@
 package by.bsac;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,21 +13,27 @@ import org.springframework.context.ApplicationContext;
 public class Main implements CommandLineRunner {
 
     private static ApplicationContext application_context;
+    private static final Logger LOGGER = LoggerFactory.getLogger(by.bsac.Main.class); //Logger
 
     public static void main(String[] args) {
 
+
         //Create SpringApplication object
+        LOGGER.info("Create application object.");
         SpringApplication application = new SpringApplication(by.bsac.Main.class);
 
         //Set application type to CLI
+        LOGGER.info("Disable web application type.");
         application.setWebApplicationType(WebApplicationType.NONE);
 
         //Disable banner
+        LOGGER.info("Disable spring banner.");
         application.setBannerMode(Banner.Mode.OFF);
 
         //Create application context
         //And
         //Run application
+        LOGGER.info("Create root application context and run application.");
         application_context = application.run(args);
     }
 
